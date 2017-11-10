a6c64f5ee7147b3138a3653ffcb5f73d1cb82497
stevemolloy <sdmolloy@gmail.com>
2017-11-10 17:04:28 +0100

    More analysing of the st.cmd file

--- analyse_stcmd.py
+++ analyse_stcmd.py
@@ -1,3 +1,7 @@
+from pathlib import PurePath
+from collections import defaultdict
+
+
 class StCmdAnalyser:
     def __init__(self, filename):
         self.filename = filename
@@ -6,13 +10,13 @@ class StCmdAnalyser:
         self.contents = [line for line in self.contents if line]
 
     @property
-    def epics_env_sets(self):
+    def epics_env_vars(self):
         retval = dict()
         for line in self.contents:
             if not line.startswith('epicsEnvSet'):
                 continue
             value_pair = self.parse_envset(line)
-            retval[value_pair[0]] = value_pair[1]
+            retval[value_pair[0].replace(',', '')] = value_pair[1]
         return retval
 
     @staticmethod
@@ -22,3 +26,34 @@ class StCmdAnalyser:
         line = line.replace(')', '')
         pairs = line.split()
         return pairs
+    
+    @property
+    def dbloadrecord_cmds(self):
+        return [line for line in self.contents if line.startswith('dbLoadRecords')]
+
+    @property
+    def templates(self):
+        template_dict = defaultdict(list)
+        for line in self.dbloadrecord_cmds:
+            template_dict[self.filename_from_command(line)].append(self.substring_from_command(line))
+        return template_dict
+
+    @staticmethod
+    def filename_from_command(cmd_str):
+        cmd_str = cmd_str.replace('dbLoadRecords("', '')
+        path = PurePath(cmd_str.split('"')[0])
+        return path.as_posix()
+
+    def substring_from_command(self, cmd_str):
+        retval = dict()
+        substitute_str = cmd_str.split('"')[-2]
+        for item in substitute_str.replace(',', ' ').split():
+            pair = item.split('=')
+            if len(pair) == 1:
+                pair.append('')
+            pair[1] = pair[1].replace('$(', '').replace(')', '')
+            try:
+                retval[pair[0]] = self.epics_env_vars[pair[1]]
+            except KeyError:
+                retval[pair[0]] = pair[1]
+        return retval
--- tests/test_stcmd_analyser.py
+++ tests/test_stcmd_analyser.py
@@ -22,5 +22,35 @@ class StCmdAnalyserTester(unittest.TestCase):
             self.assertNotIn('\n', line)
 
     def test_finding_envset_lines(self):
-        self.assertEqual(len(self.analyser.epics_env_sets), 43)
-        self.assertIsInstance(self.analyser.epics_env_sets, dict)
+        self.assertEqual(len(self.analyser.epics_env_vars), 43)
+        self.assertIsInstance(self.analyser.epics_env_vars, dict)
+        self.assertEqual(self.analyser.epics_env_vars['PORT'], 'BPM')
+        self.assertEqual(self.analyser.epics_env_vars['BPMCH14'], 'REFPHA')
+        self.assertEqual(self.analyser.epics_env_vars['EVR_PCIFUNCTION'], '0x0')
+
+    def test_find_dbloadrecord_lines(self):
+        self.assertEqual(len(self.analyser.dbloadrecord_cmds), 85)
+
+    def test_finding_template_files(self):
+        a = self.analyser
+        self.assertEqual(len(a.templates), 11)
+
+    def test_listing_substitutions(self):
+        a = self.analyser
+        self.assertEqual(a.templates['$(MRFIOC2)/db/evr-mtca-300.db'],
+                         [{'DEVICE': 'EVR', 'SYS': 'BPM', 'Link-Clk-SP': '88.0525'}])
+        self.assertEqual(a.templates["$(SIS8300)/db/SIS8300.template"],
+                         [{'P': 'BPM:', 'R': '', 'PORT': 'BPM', 'ADDR': '0', 'TIMEOUT': '1'}])
+        self.assertEqual(a.templates["$(SIS8300)/db/SIS8300N.template"],
+                         [
+                             {'P': 'BPM:', 'R': 'AI0', 'PORT': 'BPM', 'ADDR': '0', 'TIMEOUT': '1', 'NAME': 'AI0'},
+                             {'P': 'BPM:', 'R': 'AI1', 'PORT': 'BPM', 'ADDR': '1', 'TIMEOUT': '1', 'NAME': 'AI1'},
+                             {'P': 'BPM:', 'R': 'AI2', 'PORT': 'BPM', 'ADDR': '2', 'TIMEOUT': '1', 'NAME': 'AI2'},
+                             {'P': 'BPM:', 'R': 'AI3', 'PORT': 'BPM', 'ADDR': '3', 'TIMEOUT': '1', 'NAME': 'AI3'},
+                             {'P': 'BPM:', 'R': 'AI4', 'PORT': 'BPM', 'ADDR': '4', 'TIMEOUT': '1', 'NAME': 'AI4'},
+                             {'P': 'BPM:', 'R': 'AI5', 'PORT': 'BPM', 'ADDR': '5', 'TIMEOUT': '1', 'NAME': 'AI5'},
+                             {'P': 'BPM:', 'R': 'AI6', 'PORT': 'BPM', 'ADDR': '6', 'TIMEOUT': '1', 'NAME': 'AI6'},
+                             {'P': 'BPM:', 'R': 'AI7', 'PORT': 'BPM', 'ADDR': '7', 'TIMEOUT': '1', 'NAME': 'AI7'},
+                             {'P': 'BPM:', 'R': 'AI8', 'PORT': 'BPM', 'ADDR': '8', 'TIMEOUT': '1', 'NAME': 'AI8'},
+                             {'P': 'BPM:', 'R': 'AI9', 'PORT': 'BPM', 'ADDR': '9', 'TIMEOUT': '1', 'NAME': 'AI9'},
+                         ])
