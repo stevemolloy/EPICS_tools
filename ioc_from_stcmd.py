ad1506796efcd443819dcde151909c80955bfbf4
stevemolloy <sdmolloy@gmail.com>
2017-11-13 23:04:07 +0100

    Refactoring of ioc_from_stcmd.py

--- ioc_from_stcmd.py
+++ ioc_from_stcmd.py
@@ -10,15 +10,14 @@ class IocFromStCmd:
         self.stcmd_analyser = StCmdAnalyser(filename)
         self.db_files = self.get_db_files()
 
-    def get_db_files(self):
-        # print(os.environ.get('BPM'))
-        return self.get_filename_envvars()
+    @property
+    def filenames(self):
+        return self.stcmd_analyser.template_dict.keys()
 
-    def get_filename_envvars(self):
+    def get_db_files(self):
         envvars = []
-        filenames = list(self.stcmd_analyser.template_dict.keys())
         regex = r"\$\((\w+)\)"
-        for filename in filenames:
+        for filename in self.filenames:
             matches = re.finditer(regex, filename)
             for match in matches:
                 if match.group(1) in os.environ:
@@ -26,10 +25,3 @@ class IocFromStCmd:
                 else:
                     envvars.append(PurePath(filename).name)
         return envvars
-
-
-if __name__ == '__main__':
-    ioc = IocFromStCmd('tests/st.cmd')
-
-    for i in ioc.stcmd_analyser.template_dict:
-        print(i)
